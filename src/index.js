8746103e8c74248afe9fcd8cf531cfbebeeeb90e
Miralem.Ba <miralembanovi@gmail.com>
2024-02-29 18:37:19 +0100

    various changes made

--- src/index.js
+++ src/index.js
@@ -6,38 +6,60 @@ import createElement from "virtual-dom/create-element";
 const { div, button, p, h1 } = hh(h);
 
 // A combination of Tailwind classes which represent a (more or less nice) button style
-const btnStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+const btnStyle =
+  "bg-blue-500 text-white font-bold py-2 px-4 rounded transition-transform hover:-translate-y-1 hover:translate-x-1";
+
 
 // Messages which can be used to update the model
 const MSGS = {
-  UPDATE_MODEL: "UPDATE_MODEL",
-  UPDATE_RANDOM_NUMBER: "UPDATE_RANDOM_NUMBER",
+  INCREASE_MODEL: "INCREASE_MODEL",
+  DECREASE_MODEL: "DECREASE_MODEL",
   // ... ℹ️ additional messages
 };
 
 // View function which represents the UI as HTML-tag functions
 function view(dispatch, model) {
   return div({ className: "flex flex-col gap-4 items-center" }, [
-    h1({ className: "text-2xl" }, `My Title`),
-    button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_MODEL) }, "Update Model"),
-    p({ className: "text-2xl" }, `Time: ${model.currentTime}`),
-    button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_RANDOM_NUMBER) }, "Update Random Number"),
-    p({ className: "text-2xl" }, `Random Number: ${model.randomNumber}`),
-    // ... ℹ️ additional elements
+    h1({ className: "text-2xl " }, `My Counter`),
+    p({ className: "text-2xl" }, `Counter: ${model.counter}`),
+    div({ className: "flex flex-row gap-8" }, [
+      button(
+        {
+          className: btnStyle,
+          onclick: () => dispatch(MSGS.INCREASE_MODEL),
+        },
+        "Increase +"
+      ),
+      button(
+        { className: btnStyle, onclick: () => dispatch(MSGS.DECREASE_MODEL) },
+        "Decrease -"
+      ),
+    ]),
   ]);
 }
 
 // Update function which takes a message and a model and returns a new/updated model
 function update(msg, model) {
   switch (msg) {
-    case MSGS.UPDATE_MODEL:
-      return { ...model, currentTime: new Date().toLocaleTimeString() };
+    case MSGS.INCREASE_MODEL:
+      return { ...model, counter: increaseCounter(model.counter) };
 
-    case MSGS.UPDATE_RANDOM_NUMBER:
-      return { ...model, randomNumber: Math.random() };
+    case MSGS.DECREASE_MODEL:
+      return { ...model, counter: decreaseCounter(model.counter) };
+
     default:
       return model;
   }
+}
+
+// Function to increase the counter value by 1
+function increaseCounter(counter) {
+  return counter + 1;
+}
+
+// Function to decrease the counter value by 1
+function decreaseCounter(counter) {
+  return counter - 1;
 }
 
 // ⚠️ Impure code below (not avoidable but controllable)
@@ -57,8 +79,7 @@ function app(initModel, update, view, node) {
 
 // The initial model when the app starts
 const initModel = {
-  currentTime: new Date().toLocaleTimeString(),
-  randomNumber: 1,
+  counter: 0,
 };
 
 // The root node of the app (the div with id="app" in index.html)
